95c2205216ad2b6750fa8e50d54c6797f521fbec
radiumcoders <radiumcoders@gmail.com>
2025-11-14 17:43:49 +0530

    tuples

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 fn main(){
     // let 
-    let a = 1;
+    // let a = 1;
     // mut
-    let mut b = 2;
+    // let mut b = 2;
     // const 
-    const PI :f32 = 3.14159;
-   println!("let {} mutable let {} const {}",a,b,PI); 
-   b = 23;
-   println!("mutable let {}", b);
+    // const PI :f32 = 3.14159;
+   // println!("let {} mutable let {} const {}",a,b,PI); 
+   // b = 23;
+   // println!("mutable let {}", b);
+   
    // day 6 data types 
    // int
-   let small :u8 = 1;
-   let big :u128 = 234232535235325324;
-   let medium :u16 = 23423;
-   let small2 : i8 = -1;
-   let medium2 : i16 = -23423;
-   let large : i64 = -234232535235325324;
-   println!("int {} {} {} {} {} {}",small,big,medium,small2,medium2,large);
+   // let small :u8 = 1;
+   // let big :u128 = 234232535235325324;
+   // let medium :u16 = 23423;
+   // let small2 : i8 = -1;
+   // let medium2 : i16 = -23423;
+   // let large : i64 = -234232535235325324;
+   // println!("int {} {} {} {} {} {}",small,big,medium,small2,medium2,large);
+   
+   // tuples
+   let tuple =(1,23.34 , 'x');
+   let x = tuple.0;
+   let y = tuple.1;
+   let z = tuple.2;
+   println!("{y}, {z} , {x}")
    
 }
