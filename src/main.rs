210711fc0fdc40510f5122a619d65752ca4d2fc8
radiumcoders <radiumcoders@gmail.com>
2025-11-15 14:02:24 +0530

    structs enums arrays :D

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,61 @@
-fn main(){
-    // let 
+struct Person {
+    name: String,
+    age: u8,
+}
+
+//enum Light {
+//   Red,
+// Yelllow,
+//Green,
+//}
+
+fn main() {
+    // let
     // let a = 1;
     // mut
     // let mut b = 2;
-    // const 
+    // const
     // const PI :f32 = 3.14159;
-   // println!("let {} mutable let {} const {}",a,b,PI); 
-   // b = 23;
-   // println!("mutable let {}", b);
-   
-   // day 6 data types 
-   // int
-   // let small :u8 = 1;
-   // let big :u128 = 234232535235325324;
-   // let medium :u16 = 23423;
-   // let small2 : i8 = -1;
-   // let medium2 : i16 = -23423;
-   // let large : i64 = -234232535235325324;
-   // println!("int {} {} {} {} {} {}",small,big,medium,small2,medium2,large);
-   
-   // tuples
-   let tuple =(1,23.34 , 'x');
-   let x = tuple.0;
-   let y = tuple.1;
-   let z = tuple.2;
-   println!("{y}, {z} , {x}")
-   
+    // println!("let {} mutable let {} const {}",a,b,PI);
+    // b = 23;
+    // println!("mutable let {}", b);
+
+    // day 6 data types
+    // int
+    // let small :u8 = 1;
+    // let big :u128 = 234232535235325324;
+    // let medium :u16 = 23423;
+    // let small2 : i8 = -1;
+    // let medium2 : i16 = -23423;
+    // let large : i64 = -234232535235325324;
+    // println!("int {} {} {} {} {} {}",small,big,medium,small2,medium2,large);
+
+    // tuples
+    // let tuple = (1, 23.34, 'x');
+    //let x = tuple.0;
+    //let y = tuple.1;
+    //let z = tuple.2;
+    //println!("{y}, {z} , {x}");
+
+    //arrays :D
+    //let arr = [12, 23, 35, 546, 57];
+    //let first = arr[0];
+    //println!("{first}");
+
+    // structs :D
+    let person_one = Person {
+        name: String::from("jay"),
+        age: 17,
+    };
+    println!("person name {} and age {}", person_one.name, person_one.age);
+
+    //enums :D
+    // let light = Light::Red;
+    //let green = Light::Yelllow;
+    // let yellow = Light::Green;
+    //match light {
+    //  Light::Red => println!("stop!!!"),
+    //Light::Yelllow => println!("GO SLOWW!!"),
+    //Light::Green => println!("Go! Go! Go!"),
+    //}
 }
