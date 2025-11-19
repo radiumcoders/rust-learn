da94453b5ccb25839a476af8b50b4f81f8ffd13f
radiumcoders <radiumcoders@gmail.com>
2025-11-19 21:29:17 +0530

    ownership sort off

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,9 @@
-fn main() {}
+fn main() {
+    // s is not valid
 
+    let mut s = String::from("hello "); // s is valid and is on the heap
+
+    s.push_str(", World");
+
+    println!("{s}");
+} //s is no longer valid
