4be288a5e9f95e17dedceee853e8e773f37c0c65
radiumcoders <radiumcoders@gmail.com>
2025-11-22 22:47:17 +0530

    XD:

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
 fn main() {
-    let i = 5;
-    let mut s = String::from("ruster");
-    call_int(i);
-    println!("after calling fn to print value -> {i}");
-    //    call_str(s);
-    //this will give error why on day 10 XD
-    //println!("after calling fm to print value => {s}");
-    borrowing(&mut s);
-    println!("the value of s after borrowing {s}")
+    println!("hello world :D")
 }
 
-fn call_int(i: i32) {
-    println!("printing from fn {i}");
-}
-
+// fn main() {
+//     let i = 5;
+//     let mut s = String::from("ruster");
+//     call_int(i);
+//     println!("after calling fn to print value -> {i}");
+//     //    call_str(s);
+//     //this will give error why on day 10 XD
+//     //println!("after calling fm to print value => {s}");
+//     borrowing(&mut s);
+//     println!("the value of s after borrowing {s}")
+// }
+//
+// fn call_int(i: i32) {
+//     println!("printing from fn {i}");
+// }
+//
 // fn call_str(s: String) {
 //  println!("printing from fn {s}");
 // }
 
 //borrowing the variable
-fn borrowing(s: &mut String) {
-    println!("borrowing");
-    s.push_str(" world");
-}
+// fn borrowing(s: &mut String) {
+//     println!("borrowing");
+//     s.push_str(" world");
+// }
 
 //fn main() {
 //  let mut i = 5;
