c3348d9f8c2fc3b6295c5ad14bfbf6f92ef0deef
Radium Coders <radiumcoders@gmail.com>
2025-12-03 21:30:39 +0530

    enums more enums .. !'-'

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
+// #[derive(Debug)]
+// enum Direction {
+//     Up,
+//     Down,
+//     Left,
+//     Right,
+// }
+
+// #[derive(Debug)]
+// struct Home {
+//     address: String,
+//     direction:Direction,
+// }
+
+// fn main () {
+//     let up = Direction::Up;
+//     println!("{:?}", up);
+//     let home = Home  {
+//         address: String::from("1234, marconi street"),
+//         direction: Direction::Up
+//     };
+//     println!("{:?}", home);
+// }
 #[derive(Debug)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
+enum Methods{
+    Create(String),
+    Locate(i32),
+    Quit
 }
 
-#[derive(Debug)]
-struct Home {
-    address: String,
-    direction:Direction,
+impl Methods {
+    fn call (&self) {
+        println!("{:?}",self)
+    }
 }
+
 
 fn main () {
-    let up = Direction::Up;
-    println!("{:?}", up);
-    let home = Home  {
-        address: String::from("1234, marconi street"),
-        direction: Direction::Up
-    };
-    println!("{:?}", home);
+    let g = Methods::Create(String::from("Hello"));
+    g.call();
 }
