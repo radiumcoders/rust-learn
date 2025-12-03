e3f3442bf56f6ce481f29ca2b95a0d974a4ba29c
Radium Coders <radiumcoders@gmail.com>
2025-12-03 19:21:51 +0530

    enums and with struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+struct Home {
+    address: String,
+    direction:Direction,
+}
+
 fn main () {
-    println!("Hello, world!");
+    let up = Direction::Up;
+    println!("{:?}", up);
+    let home = Home  {
+        address: String::from("1234, marconi street"),
+        direction: Direction::Up
+    };
+    println!("{:?}", home);
 }
