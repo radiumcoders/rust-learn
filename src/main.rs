484dd9bda7904fcb146c4dec0ed6913916856ebc
Radium Coders <radiumcoders@gmail.com>
2025-12-14 19:16:51 +0530

    file operation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,1 @@
-// check if the number is palindrome or not without cinverting to string
-
-fn main() {
-    let num = -12341;
-    let mut rev = 0;
-    let mut n = num;
-    if num < 0 {
-        return false;
-    } else {
-        while n > 0 {
-            let rem = n % 10; // extract the last digit
-            n = n / 10; // remove the last digit
-            rev = rev * 10 + rem; // add the last digit in the reversed
-        }
-    }
-    return num == rev;
-}
+fn main () {}
