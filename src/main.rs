27e25b45d4148eca714bd1c5b49fb077f8353c64
radiumcoders <radiumcoders@gmail.com>
2026-01-24 14:58:01 +0530

    traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+//traits we are going to learn :)
+#[derive(Debug)]
+struct Tweet {
+    username: String,
+    body: String,
+    on: u32,
+}
+
+trait Summary {
+    fn summary(&mut self) -> String;
+}
+
+impl Summary for Tweet {
+    fn summary(&mut self) -> String {
+        self.on += 1;
+        format!("{}: {}", self.username, self.body)
+    }
+}
+
 fn main() {
-    
+    let mut teww = Tweet {
+        username: String::from("RadiumCoders"),
+        body: String::from("Learning Rust traits is fun!"),
+        on: 0,
+    };
+    println!("Tweet Summary: {}", teww.summary());
 }
